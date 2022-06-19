9082c1313400ccd6ef8382bb908d0a57f1eb33e6
Galu199 <Seweryngalu199@gmail.com>
2022-06-19 17:11:00 +0200

    Delete datasets directory

--- datasets/detectCustomModel.ps1
+++
@@ -1,7 +0,0 @@
-set-location "./yolov5"
-$img = 640 #1280
-$weights2 = "runs\train\exp19\weights\last.pt" #"runs\train\exp9\weights\best.pt"
-$source = "test2.mkv"
-
-python detect.py --img $img --weights $weights2 --source $source
-timeout /t -1
